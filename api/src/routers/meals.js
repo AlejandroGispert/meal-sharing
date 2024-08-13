f6d493842f04425e7f9aadee94f40b4c83ae6340
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-08-13 23:29:58 +0200

    created all reservations

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -1,4 +1,5 @@
 import express from "express";
+import knex from "../database_client.js";
 
 // This router can be deleted once you add your own router
 const mealsRouter = express.Router();
--- api/src/routers/reservations.js
+++ api/src/routers/reservations.js
@@ -1,10 +1,25 @@
 import express from "express";
-
+import knex from "../database_client.js";
 // This router can be deleted once you add your own router
 const reservationsRouter = express.Router();
 
-reservationsRouter.get("/", (req, res) => {
-  res.json({ message: "Hello nested router" });
+reservationsRouter.get("/api/reservations", async (req, res) => {
+  const allReservations = await knex
+    .select("*")
+    .from("Meal")
+    .orderBy("ID ASC")
+    .then((rows) => {
+      console.log(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  if (allReservations.length > 0) {
+    res.send(allReservations);
+  } else {
+    res.send([]);
+  }
 });
 
 export default reservationsRouter;
